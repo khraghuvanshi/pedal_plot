deb6682f801bc964b05d1c7fce83ea929bf90306
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-02-24 18:23:48 -0800

    Update map.js

--- map.js
+++ map.js
@@ -2,14 +2,14 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
 console.log("Mapbox GL JS Loaded:", mapboxgl);
 // Set your Mapbox access token here
-mapboxgl.accessToken = 'pk.eyJ1IjoicmtodXNoaSIsImEiOiJjbTdjanBrNGswaGloMm5vZDJnM21xNmo2In0.JODYoQmb2JzLJbsQP0E4yQ';
+// mapboxgl.accessToken = 'pk.eyJ1IjoicmtodXNoaSIsImEiOiJjbTdjanBrNGswaGloMm5vZDJnM21xNmo2In0.JODYoQmb2JzLJbsQP0E4yQ';
 
-// Initialize the map
-const map = new mapboxgl.Map({
-  container: 'map', // ID of the div where the map will render
-  style: 'mapbox://styles/mapbox/streets-v12', // Map style
-  center: [-71.09415, 42.36027], // [longitude, latitude]
-  zoom: 12, // Initial zoom level
-  minZoom: 5, // Minimum allowed zoom
-  maxZoom: 18 // Maximum allowed zoom
-});
+// // Initialize the map
+// const map = new mapboxgl.Map({
+//   container: 'map', // ID of the div where the map will render
+//   style: 'mapbox://styles/mapbox/streets-v12', // Map style
+//   center: [-71.09415, 42.36027], // [longitude, latitude]
+//   zoom: 12, // Initial zoom level
+//   minZoom: 5, // Minimum allowed zoom
+//   maxZoom: 18 // Maximum allowed zoom
+// });
