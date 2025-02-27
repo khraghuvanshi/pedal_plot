5b5b28d3fc8c6d5d11b109abc0cc53d5f535bfb7
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-02-26 19:29:08 -0800

    Update map.js

--- map.js
+++ map.js
@@ -120,7 +120,13 @@ map.on('load', async () => {
       .attr('fill', 'steelblue')  // Circle fill color
       .attr('stroke', 'white')    // Circle border color
       .attr('stroke-width', 1)    // Circle border thickness
-      .attr('opacity', 0.8);      // Circle opacity
+      .attr('opacity', 0.8)      // Circle opacity
+      .each(function(d){
+        d3.select(this)
+          .append('title')
+          .text('${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)');
+      })
+      
 
 
       
